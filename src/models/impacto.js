091f42ef6db8c7f0dc067413ad22912b8c10e50e
unknown <chavarria.alejandro@outlook.com>
2022-08-17 14:38:02 -0600

    Tablas en plural

--- src/models/impacto.js
+++ src/models/impacto.js
@@ -1,5 +1,5 @@
 module.exports = function({Id,Nivel, Puntaje, Descripcion}) {
- let db= 'Impacto';
+ let db= 'Impactos';
     return ({
         Id,Nivel,Puntaje,Descripcion,
         queryGet:`SELECT * FROM ${db};`,
