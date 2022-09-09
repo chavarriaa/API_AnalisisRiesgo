4451b820fcba3cc61119b31baddb15ff64c27bc6
Rafael Alejandro Chavarría <chavarria.alejandro@outlook.com>
2022-09-08 20:07:16 -0600

    arreglado planSeguridad ->PlanSeguridad

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/', require('./src/routes/posibilidad'));
 app.use('/', require('./src/routes/riesgo'));
 app.use('/', require('./src/routes/activo'));
 app.use('/', require('./src/routes/riesgoActivo'));
-app.use('/', require('./src/routes/planSeguridad'));
+app.use('/', require('./src/routes/PlanSeguridad'));
 app.use('/', require('./src/routes/planAccion'));
 app.use('/', require('./src/routes/incidente'));
 app.use('/', require('./src/routes/planRespuesta'));
